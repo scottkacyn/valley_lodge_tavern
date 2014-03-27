92f3ff9e7083cc106dee099f0edcd678620174e5
Prelang Builder <builder@prelang.com>
2014-03-27 19:07:37 +0000

    Initial commit; clean Rails application

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+ValleyLodgeTavern::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ValleyLodgeTavern::Application.config.session_store :cookie_store, key: '_valley_lodge_tavern_session'
