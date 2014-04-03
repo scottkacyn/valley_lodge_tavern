ebf85b087025d7ac6e93e7db3b660e29e179c947
Scott Kacyn <scott.kacyn@gmail.com>
2014-04-03 09:03:59 -0500

    Added some images

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,4 +1,22 @@
 class LandingsController < ApplicationController
+  before_filter :allow_iframe_requests
+
   def index
+  end
+
+  def menu
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def location
+  end
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
   end
 end
